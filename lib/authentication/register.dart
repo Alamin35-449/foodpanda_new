7f32955c128e9e55fbc017a4799e847868d83f69
Md Alamin <alamin35-449@diu.edu.bd>
2023-01-31 13:15:04 +0600

    Initial commit

---
+++ lib/authentication/register.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class register_screen extends StatefulWidget {
+  const register_screen({Key? key}) : super(key: key);
+
+  @override
+  State<register_screen> createState() => _register_screenState();
+}
+
+class _register_screenState extends State<register_screen> {
+
+  TextEditingController anycontroller=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+
+
+
+    );
+  }
+}
